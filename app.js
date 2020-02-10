70a816c4a6975b35f7b1723d97af69ca277ee53d
Naomi Sharp <naomisharp@hotmail.co.uk>
2020-02-10 10:05:34 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,54 @@
+var express = require('express'),
+    app     = express();
+
+app.use(express.static(__dirname + "/public"));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/trees', (req, res) => {
+  res.render('trees');
+});
+
+app.get('/flowers', (req, res) => {
+  res.render('flowers');
+});
+
+app.get('/birds', (req, res) => {
+  res.render('birds');
+});
+
+app.get('/horses', (req, res) => {
+  res.render('horses');
+});
+
+app.get('/bees', (req, res) => {
+  res.render('bees');
+});
+
+app.get('/beaches', (req, res) => {
+  res.render('beaches');
+});
+
+app.get('/stars', (req, res) => {
+  res.render('stars');
+});
+
+app.get('/mountains', (req, res) => {
+  res.render('mountains');
+});
+
+app.get('/caves', (req, res) => {
+  res.render('caves');
+});
+
+app.get('/sky', (req, res) => {
+  res.render('sky');
+});
+
+
+app.listen(3000, function(){
+  console.log('Server listening')
+});
